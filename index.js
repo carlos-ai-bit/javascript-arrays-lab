6c329c935320f08be0e8df7d604c78503074fb5c
Hoperaisers <abdallahsafari@gmail.com>
2023-02-24 13:38:45 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-var kittens = [] //define your array here
+var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
 
 // Add your functions and code here
+function destructivelyAppendKitten(name) {
+    kittens.push(name)
+}
+function destructivelyPrependKitten(name) {
+    kittens.unshift(name)
+}
+function destructivelyRemoveLastKitten(name) {
+    kittens.pop(name)
+}
+function destructivelyRemoveFirstKitten(name) {
+    kittens.shift(name)
+}
+function appendKitten(name) {
+    return kittens.concat(name);
+}
+function prependKitten(name) {
+    const newKittens = [name, ...kittens];
+    return newKittens;
+}
+function removeLastKitten(name) {
+    const cats = kittens.slice(0, kittens.length - 1);
+    return cats
+}
+function removeFirstKitten(name) {
+    const cats = kittens.slice(1)
+    return cats
+}
